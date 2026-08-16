20ee124a17088f5fafe5a3ce6dd218142553fd30
Burning Turtle <git@burningturtle.win>
2026-08-16 11:43:05 +0200

    added global config, imported by hosts

--- hosts/hp/configuration.nix
+++ hosts/hp/configuration.nix
@@ -2,114 +2,29 @@
 { inputs, config, pkgs, ... }:
 
 {
-  environment.variables = {
-    HARDWARE = "hp";
-    EDITOR = "nvim";
-    SOPS_AGE_SSH_PRIVATE_KEY_FILE="/etc/ssh/ssh_host_ed25519_key";
-  };
-
+  
   home-manager.users.burningturtle = {
     my.sway.eDP_scale = "1.0";
   };
 
+  environment.variables = {
+    HARDWARE = "hp";
+  };
+
+
   imports =
     [
       ./hardware-configuration.nix
-      ../../modules/default.nix
+      ../../global_config.nix
     ];
-
-  sops = {
-    defaultSopsFile = ../../secrets/secrets.yaml;
-    age.keyFile = "/var/lib/sops-nix/key.txt";
-
-    secrets."wifi/KabelBox-4F28" = {};
-  };
-
-  boot.loader = {
-    grub = {
-      enable = true;
-      device = "nodev";
-      efiSupport = true;
-    };
-    efi.canTouchEfiVariables = true;
-  };
-
-  # Use latest kernel.
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-
-  security.polkit = {
-    enable = true;
-    enablePkexecWrapper = true;
-  };
-
+  
   networking.hostName = "nixos"; # Define your hostname.
 
-  # Enable networking
-  networking.networkmanager.enable = true;
-  systemd.services.NetworkManager = {
-    after = [ "sops-nix.service" ];
-    wants = [ "sops-nix.service" ];
-  };
-
-  # Time zone.
-  time.timeZone = "Europe/Berlin";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "de_DE.UTF-8";
-    LC_IDENTIFICATION = "de_DE.UTF-8";
-    LC_MEASUREMENT = "de_DE.UTF-8";
-    LC_MONETARY = "de_DE.UTF-8";
-    LC_NAME = "de_DE.UTF-8";
-    LC_NUMERIC = "de_DE.UTF-8";
-    LC_PAPER = "de_DE.UTF-8";
-    LC_TELEPHONE = "de_DE.UTF-8";
-    LC_TIME = "de_DE.UTF-8";
-  };
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = false;
-
-  # Configure keymap in X11
-  #services.xserver.xkb = {
-  #  layout = "us";
-  #  variant = "intl";
-  #};
-
-  # Configure console keymap
-  console.keyMap = "us-acentos";
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Enable fish shell (registers it in /etc/shells and provides completions).
-  programs.fish.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+    # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users."burningturtle" = {
     isNormalUser = true;
     description = "BurningTurtle";
     extraGroups = [ "networkmanager" "wheel" ];
     shell = pkgs.fish;
   };
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # enable experimental features
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # Garbage collection
-  nix.gc.automatic = true;
-  
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "26.05"; # Did you read the comment?
-
 }
