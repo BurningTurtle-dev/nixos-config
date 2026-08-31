4bb6b9f94ddd650924bcfc907a82a45b56ed6ecb
BurningTurtle <git@burningturtle.win>
2026-08-31 12:55:04 +0200

    more cleanup

--- flake.nix
+++ flake.nix
@@ -2,24 +2,18 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nur = {
-      url = "github:nix-community/NUR";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixvim = { url = "github:nix-community/nixvim"; };
-    nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
-    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
     stylix = {
       url = "github:nix-community/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, nur, nixvim, nix-flatpak, sops-nix, nix-vscode-extensions, stylix, ... }:
+  outputs = { self, nixpkgs, home-manager, nixvim, sops-nix, stylix, ... }:
     let
       system = "x86_64-linux";
     in {
@@ -33,14 +27,13 @@
 
             home-manager.nixosModules.home-manager
             {
-              nixpkgs.overlays = [ nur.overlays.default ];
+              nixpkgs.overlays = [ ];
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.users.user = {
                 imports = [
                   ./home.nix
                   nixvim.homeModules.nixvim
-		  nix-flatpak.homeManagerModules.nix-flatpak
                 ];
               };
             }
