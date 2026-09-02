a81b9d25b728a59182310b81b91abb2aedee0758
BurningTurtle <git@burningturtle.win>
2026-09-03 01:01:46 +0200

    flake update timer + upgrade

--- modules/services/auto-upgrade.nix
+++ modules/services/auto-upgrade.nix
@@ -1,6 +1,23 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  systemd.services.flake-update = {
+    description = "Update flake.lock";
+    serviceConfig.Type = "oneshot";
+    script = ''
+      cd /etc/nixos
+      ${pkgs.nix}/bin/nix flake update --commit-lock-file
+    '';
+  };
+
+  systemd.timers.flake-update = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "Sun *-*-* 03:30";  # 30 min before the rebuild
+      Persistent = true;
+    };
+  };
+
   system.autoUpgrade = {
     enable = true;
     flake = "/etc/nixos#thinkcentre";
@@ -8,7 +25,7 @@
       "--print-build-logs"
       "--commit-lock-file"  # If you want to automatically commit the updated flake.lock
     ];
-    dates = "04:00";
+    dates = "Sun *-*-* 04:00";
     randomizedDelaySec = "45min";
   };
 }
